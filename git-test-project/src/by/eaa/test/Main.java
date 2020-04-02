c73108d4977bb8ea51bcf45e03b81359b19caf24
zazadupond <mTsy7H7494vQNdTnUoTw>
2020-04-02 13:31:37 +0300

    test2

--- git-test-project/src/by/eaa/test/Main.java
+++ git-test-project/src/by/eaa/test/Main.java
@@ -3,6 +3,8 @@ package by.eaa.test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello from Git Hub!!!");
+		System.out.println("Hello from Git Hub!!!");
+		System.out.println("Hello from Git Hub!!!");
 	}
 
 }
